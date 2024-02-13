f4ec8ab4cf4781d97bfb84f6dda15314af0f52dd
Juan <juangui9508@hotmail.com>
2024-02-13 11:42:08 -0500

    feat():responsive styles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,33 +1,57 @@
 
-import Juan from '../../assets/Juan.jpg'
+// import Juan from '../../assets/Juan.jpg'
 
-const About = () =>{
-  // const imagen1 = 'https://shorturl.at/tuzG0';
-   const imagen1  = Juan;
+// const About = () =>{
+//   // const imagen1 = 'https://shorturl.at/tuzG0';
+//    const imagen1  = Juan;
 
- return (
-    <div className = 'grid gap-4 grid-cols-2 relative mt-16 ml-40'>
+//  return (
+//     <div className = 'grid gap-4 grid-cols-2 relative mt-16 ml-40'>
     
-      <div>
-      <p className =' font-Protest-Revolution text-cyan-50  text-4xl text-center'>Welcome to my portfolio!</p>
-      <br></br>
-      <p className = "text-cyan-50 text-2xl ml-4 mr-16 justify-center font-Changa" >
-      I am an electronic engineer and 
-      software developer with a wide range of technical skills ranging 
-      from electronics to web development. My passion for technology 
-      has led me to acquire a diverse set of competencies that allow me to 
-      approach complex projects from multiple perspectives.</p>
-      </div>
+//       <div>
+//       <p className =' font-Protest-Revolution text-cyan-50  text-4xl text-center'>Welcome to my portfolio!</p>
+//       <br></br>
+//       <p className = "text-cyan-50 text-2xl ml-4 mr-16 justify-center font-Changa" >
+//       I am an electronic engineer and 
+//       software developer with a wide range of technical skills ranging 
+//       from electronics to web development. My passion for technology 
+//       has led me to acquire a diverse set of competencies that allow me to 
+//       approach complex projects from multiple perspectives.</p>
+//       </div>
 
-      <div>
-      <img className=" w-56 h-64 rounded-3xl mt-2 "  src={imagen1} alt="imagen de perfil"/> 
-      </div>    
+//       <div>
+//       <img className=" w-56 h-64 rounded-3xl mt-2 "  src={imagen1} alt="imagen de perfil"/> 
+//       </div>    
        
 
-    </div>
+//     </div>
     
- )
+//  )
 
+// };
+
+// export default About;
+
+import Juan from '../../assets/Juan.jpg'
+
+const About = () => {
+   const imagen1 = Juan;
+
+   return (
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 relative mt-16 ml-4 sm:ml-40">
+         <div>
+            <p className='font-Protest-Revolution text-cyan-50 text-4xl text-center'>Welcome to my portfolio!</p>
+            <br></br>
+            <p className="text-cyan-50 text-2xl ml-4 sm:ml-0 mr-16 sm:mr-0 justify-center font-Changa">
+               I am an electronic engineer and software developer with a wide range of technical skills ranging from electronics to web development. My passion for technology has led me to acquire a diverse set of competencies that allow me to approach complex projects from multiple perspectives.
+            </p>
+         </div>
+
+         <div>
+            <img className="w-full h-auto rounded-3xl mt-2 sm:w-56 sm:h-64" src={imagen1} alt="imagen de perfil" />
+         </div>
+      </div>
+   );
 };
 
 export default About;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,47 @@
 
 
-const Contact  = () =>{
+// const Contact  = () =>{
 
+//     const whatsapp = "https://shorturl.at/dilTY ";
+//     const linkedin = "https://shorturl.at/hCJMZ";
+//     const github = "https://shorturl.at/gnO27";
+//     const email = "https://shorturl.at/cK347";
+//     const whatsapp_url = "https://wa.me/+573146061549";
+//     const email_url = "mailto:juangui9508@hotmail.com";
+//     const linkedin_url ="https://www.linkedin.com/in/juan-guillermo-ing-electronico/";
+//     const github_url = "https://github.com/juaneraso";
+
+//     return(
+//         <div className='bg-blue-950 border border-blue-950'>
+        
+//           <h1 className='  text-center mt-2 text-4xl text-white font-Changa mb-3 '>Contact</h1>   
+            
+//            <div className= 'flex justify-center' >
+//               <a href={whatsapp_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4'  src={whatsapp} alt="imagen_whatsapp"/></a>
+            
+//               <a href={linkedin_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4' src={linkedin} alt="imagen_linkedin"/></a>
+               
+         
+//               <a href={github_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4' src={github} alt="imagen_github"/></a>
+           
+//               <a href={email_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4' src={email} alt="imagen_email"/></a>
+
+//            </div>               
+
+       
+//         <h1 className='font-Protest-Revolution text-center text-3xl mt-6 mb-4 text-white'>Juan Guillermo - 2024 </h1>
+      
+                           
+           
+//      </div>
+
+//     )
+// };
+
+
+// export default Contact;
+
+const Contact = () => {
     const whatsapp = "https://shorturl.at/dilTY ";
     const linkedin = "https://shorturl.at/hCJMZ";
     const github = "https://shorturl.at/gnO27";
@@ -11,36 +51,32 @@ const Contact  = () =>{
     const linkedin_url ="https://www.linkedin.com/in/juan-guillermo-ing-electronico/";
     const github_url = "https://github.com/juaneraso";
 
-    return(
-        <div className='bg-blue-950 border border-blue-950'>
-        
-          <h1 className='  text-center mt-2 text-4xl text-white font-Changa mb-3 '>Contact</h1>   
-            
-           <div className= 'flex justify-center' >
-              <a href={whatsapp_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4'  src={whatsapp} alt="imagen_whatsapp"/></a>
-            
-              <a href={linkedin_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4' src={linkedin} alt="imagen_linkedin"/></a>
-               
-         
-              <a href={github_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4' src={github} alt="imagen_github"/></a>
-           
-              <a href={email_url} target="_blank" rel="noopener noreferrer"><img className='w-24 h-24 mr-4 mt-4' src={email} alt="imagen_email"/></a>
-
-           </div>
-        
-           
-
-       
-        <h1 className='font-Protest-Revolution text-center text-3xl mt-6 mb-4 text-white'>Juan Guillermo - 2024 </h1>
-      
-        
-        
-             
-           
+ 
+   return (
+     <div className="bg-blue-950 border border-blue-950 py-8">
+       <h1 className="text-center mt-2 text-4xl text-white font-Changa mb-3">Contact</h1>
+ 
+       <div className="flex justify-center items-center flex-wrap">
+         <a href={whatsapp_url} target="_blank" rel="noopener noreferrer" className="mx-4 my-2">
+           <img className="w-24 h-24" src={whatsapp} alt="imagen_whatsapp" />
+         </a>
+ 
+         <a href={linkedin_url} target="_blank" rel="noopener noreferrer" className="mx-4 my-2">
+           <img className="w-24 h-24" src={linkedin} alt="imagen_linkedin" />
+         </a>
+ 
+         <a href={github_url} target="_blank" rel="noopener noreferrer" className="mx-4 my-2">
+           <img className="w-24 h-24" src={github} alt="imagen_github" />
+         </a>
+ 
+         <a href={email_url} target="_blank" rel="noopener noreferrer" className="mx-4 my-2">
+           <img className="w-24 h-24" src={email} alt="imagen_email" />
+         </a>
+       </div>
+ 
+       <h1 className="text-center text-3xl mt-6 mb-4 text-white">Juan Guillermo - 2024</h1>
      </div>
-
-    )
-};
-
-
-export default Contact;
+   );
+ };
+ 
+ export default Contact;
